acff1ae8341b5136796d5aac75c7ec435175e712
Richie S <richie.shii@gmail.com>
2022-05-05 22:49:36 -0400

    hello world command

--- src/bot.js
+++ src/bot.js
@@ -5,7 +5,15 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`)
-  })
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
-client.login(process.env.BOT_TOKEN)
+const prefix = '!';
+client.on("messageCreate", message => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  message.channel.send(`You said "${message.content}"`);
+  message.reply("hi")
+});
+
+client.login(process.env.BOT_TOKEN);
